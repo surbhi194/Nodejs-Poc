4fc6cea07f11943c592d60a639c84aa708e76990
Surbhi gupta <surbhigupta100502@gmail.com>
2023-03-07 01:06:47 +0530

    done delete and patch api

--- src/app.js
+++ src/app.js
@@ -7,11 +7,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-// create a new students
-app.get("/students", (req, res) => {
-    res.send("hello world");
-})
 
+// create a new students
 app.post("/students", (req, res) => {
     console.log(req.body);
     const user = new Student(req.body);
@@ -22,8 +19,59 @@ app.post("/students", (req, res) => {
         console.log("error" + e);
         res.status(400).send(e);
     })
+})
 
-    // res.send("hello from student side");
+//get student by id
+app.get("/students", async(req,res) =>{
+    try{
+       const studentsData =await Student.find();
+       res.send(studentsData);
+    }catch(e){
+        res.send(e);
+    }
+})
+
+//get all students
+// app.get("/students/:id", async(req,res) =>{
+//     try{
+//         const _id = req.params.id;
+//        const studentData =await Student.findById(_id);
+//        if(!studentData){
+//         return res.status(404).send();
+//        }else{
+//         res.send(studentData);
+//        }
+       
+//     }catch(e){
+//         res.status(404).send(e);
+//     }
+// })
+
+//Update the student
+app.patch("/students/:id", async(req,res) =>{
+    try{
+        const _id = req.params.id;
+       const updateStudents =await Student.findByIdAndUpdate(_id, req.body, {
+        new: true
+       });
+       res.send(updateStudents);
+    }catch(e){
+        res.status(404).send(e);
+    }
+})
+
+//delete the student  by id
+app.delete("/students/:id", async(req,res) =>{
+    try{
+        const _id = req.params.id;
+       const deleteStudents =await Student.findByIdAndDelete(_id);
+       if(!_id){
+        return res.status(400).send();
+       }
+       res.send(deleteStudents);
+    }catch(e){
+        res.status(404).send(e);
+    }
 })
 
 const uri = "mongodb+srv://Surbhi_194:Surbhi_194@cluster0.etezf8c.mongodb.net/studentapi?retryWrites=true&w=majority"
